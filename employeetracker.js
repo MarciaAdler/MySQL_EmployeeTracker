866b3ff82926416e87e5e63acb59ffad7084a49d
Marcia <marciaadler718@gmail.com>
2020-02-07 17:28:40 -0500

    Added some add employee functionality

--- employeetracker.js
+++ employeetracker.js
@@ -23,16 +23,14 @@ connection.connect(function(err) {
 });
 
 function afterConnection() {
-  let roles = roles.map((role, index) => {
-    return roles;
-  });
   start();
-  //   connection.query("SELECT * FROM products", function(err, res) {
-  //     if (err) throw err;
-  //     console.log(res);
-  //connection.end();
-  //   });
 }
+
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//connection.end();
+//   });
 
 function start() {
   inquirer
@@ -58,12 +56,22 @@ function start() {
       console.log(res.action);
       if (res.action === "Add Employee") {
         addEmployee();
-      } else if (res.action === "View All Employees") {
-        viewEmployees();
+        //   } else if (res.action === "View All Employees") {
+        //     viewEmployees();
       }
     });
 }
-function addEmployee(firstName, lastName, roleId, managerId) {
+function addEmployee() {
+  const managerList = [];
+  connection.query(
+    "SELECT first_name, last_name FROM employees INNER JOIN roles ON employees.manager_id = roles.id WHERE title = 'Manager'",
+    (err, res) => {
+      for (let i = 0; i < res.length; i++) {
+        let fullName = res[i].first_name + " " + res[i].last_name;
+        managerList.push(fullName);
+      }
+    }
+  );
   inquirer
     .prompt([
       {
@@ -75,29 +83,69 @@ function addEmployee(firstName, lastName, roleId, managerId) {
         type: "input",
         name: "lastName",
         message: "What is the employee's last name?"
-      },
-      {
-        type: "list",
-        name: "roleId",
-        message: "What is the employee's role?",
-        choices: roles
-      },
-      {
-        type: "input",
-        name: "managerId",
-        message: "Who is the employee's manager?"
-        // choices: ""
       }
     ])
     .then(function(res) {
-      console.log(roles);
-      //   const query = connection.query("INSERT INTO employee SET ?", {
-      //     first_name: res.firstName,
-      //     last_name: res.lastName,
-      //     role_id: res.roleId,
-      //     manager_id: res.managerId
-      //   });
-
-      start();
+      console.log(res);
+      const query = connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: res.firstName,
+          last_name: res.lastName
+        },
+        (err, res) => {
+          console.log(res);
+          const id = res.insertId;
+          updateRoleId(id);
+        }
+      );
     });
 }
+function updateRoleId(id) {
+  let roles = [];
+  connection.query("SELECT title FROM roles", (err, res) => {
+    for (let i = 0; i < res.length; i++) {
+      roles.push(res[i].title);
+    }
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "roleId",
+          message: "What is the employee's role?",
+          choices: roles
+        }
+      ])
+      .then(function(res) {
+        console.log(res.roleId);
+        const query = `SELECT id FROM roles WHERE title = '${res.roleId}'`;
+
+        connection.query(query, function(err, res) {
+          console.log(res[0].id);
+          const query = `UPDATE employees SET role_id = '${res[0].id}' WHERE employees.id = '${id}'`;
+          connection.query(query, function(err, res) {
+            console.log(res);
+          });
+        });
+      });
+  });
+}
+
+//
+//     {
+//       type: "list",
+//       name: "manager",
+//       message: "Who is the employee's manager?",
+//       choices: managerList
+//     }.then(function(res) {
+//       console.log(res);
+//   const query = connection.query("INSERT INTO employees SET ?", {
+//     first_name: res.firstName,
+//     last_name: res.lastName,
+//     role_id: res.roleId,
+//     manager_id: res.managerId
+//   });
+
+//start();
+//});
--- employeetracker.sql
+++ employeetracker.sql
@@ -8,13 +8,13 @@ CREATE TABLE employees (
   id INTEGER(10) AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER(10) NOT NULL,
+  role_id INTEGER(10),
   manager_id INTEGER(10),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INTEGER(10) AUTO_INCREMEMT NOT NULL,
+  id INTEGER(10) AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INTEGER(10),
@@ -22,11 +22,16 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE departments (
-  id INTEGER(10) AUTO_INCREMEMT NOT NULL,
+  id INTEGER(10) AUTO_INCREMENT NOT NULL,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
--- INSERT INTO user (username, firstName, lastName)
--- VALUES ("Franklintendo", "Frank", "Lintendo"), ("EllenP", "Ellen", "Park")
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", "90000", "1"), ("HR Business Partner", "95000", "2")
 
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Manager", "125000", "1"), ("Manager", "125000", "2")
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Cher","Morabito", "1", "4"), ("Gail", "Bernstein" ,"2", "5"),
